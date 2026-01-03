c4e5011ece0bae025595d8a250a29469a033713c
bochiers <bochiers123@gmail.com>
2026-01-04 00:57:10 +0700

    change navbar to Center

--- wiztour/components/Navbar.tsx
+++ wiztour/components/Navbar.tsx
@@ -11,24 +11,23 @@ const Navbar = () => {
         { name: 'Car Gallery', path: '/car-gallery' },
         { name: 'Destinations', path: '/destinations' },
         { name: 'Pricing', path: '/pricing' },
-        { name: 'Contact', path: '/contact' },
+        { name: 'About Us', path: '/about-us' },
     ];
     return ( 
         // untuk return navbar
         <nav className='bg-[#E9E3D7] p-2 shadow-md w-full h-[80px] flex items-center'>
-            <div className='container m-4 flex justify-start'>
+            <div className='container m-4 flex justify-center'>
                 <div className='space-x-10'>
                     {navLinks.map((link) => {
                         const isActive = pathname === link.path; 
-                        //untuk memanggil koktak is active
                         return (
                             <Link 
                                 key={link.name}
                                 href={link.path}
-                                className={`p-4 rounded-xl transition-all-duration-300
+                                className={`px-4 py-2 rounded-lg font-medium transition-all duration-300 
                                         ${isActive 
-                                        ? "bg-white/30 backdrop-blur-lg border border-white/40 shadow-[0_4px_30px_rgba(0,0,0,0.1)] text-blue-700 font-normal" 
-                                        : "text-gray-600 hover:bg-white/20 hover:backdrop-blur-sm"
+                                        ? "underline text-red-700 bg-red-100/50" 
+                                        : "text-gray-600 hover:text-red-600 hover:bg-red-50/50 hover:shadow-sm"
                                         }
                                     `}
                             >
