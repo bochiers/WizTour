c3643128259e5baa9eb4be0ceae6319d225d5077
bochiers <bochiers123@gmail.com>
2026-01-03 22:48:44 +0700

    first commit

---
+++ wiztour/components/Navbar.tsx
@@ -0,0 +1,47 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'; //untuk mengarahkan ke halaman lain
+const Navbar = () => {
+    const pathname = usePathname();
+    
+    const navLinks = [
+        { name: 'Home', path: '/' },
+        { name: 'About', path: '/about' },
+        { name: 'Packages', path: '/packages' },
+        { name: 'Car Gallery', path: '/car-gallery' },
+        { name: 'Destinations', path: '/destinations' },
+        { name: 'Pricing', path: '/pricing' },
+        { name: 'Contact', path: '/contact' },
+    ];
+    return ( 
+        // untuk return navbar
+        <nav>
+            <div className='container m-4 flex justify-start'>
+                <div className='space-x-10'>
+                    {navLinks.map((link) => {
+                        const isActive = pathname === link.path; 
+                        //untuk memanggil koktak is active
+                        return (
+                            <Link 
+                                key={link.name}
+                                href={link.path}
+                                className={`padding-4 rounded-xl transition-all-duration-300
+                                        ${isActive 
+                                        ? "bg-white/30 backdrop-blur-lg border border-white/40 shadow-[0_4px_30px_rgba(0,0,0,0.1)] text-blue-700 font-normal" 
+                                        : "text-gray-600 hover:bg-white/20 hover:backdrop-blur-sm"
+                                        }
+                                    `}
+                            >
+                                {link.name}
+                            </Link>
+                        );
+                    })}
+                </div>
+                
+            </div>
+        </nav>
+        );
+}
+
+export default Navbar;
+
