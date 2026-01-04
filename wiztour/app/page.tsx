7403b098dcc3a045b6cd9ffdbd72cdd3c57b52cf
bochiers <bochiers123@gmail.com>
2026-01-04 13:13:15 +0700

    add footer

--- wiztour/app/page.tsx
+++ wiztour/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
     {/* <h1>Hello World</h1> */}
-    <Hero />
+    <Hero/>
+    <Footer/>
     </>
   );
 }
---
+++ wiztour/components/Footer.tsx
@@ -0,0 +1,13 @@
+'use client';
+export default function Footer() {
+    return (
+        <section className="bg-gray-800 text-white py-6 mt-10">
+            <div className="container mx-auto text-center">
+                <a href="https://www.linkedin.com/in/nadwyanshary/" className="text-sm hover:underline">
+                    &copy; {new Date().getFullYear()} 
+                    Made by Love 
+                </a>
+            </div>
+        </section>
+    );
+}
